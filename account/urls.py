e3a750f362cb2814afbf02a055a63521c0cddb86
junior <junior4g4@gmail.com>
2017-07-10 10:51:04 -0300

    Profile

--- account/urls.py
+++ account/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     url(r'^login/$', login, {'template_name': 'login.html'}, name='login'),
     url(r'^logout/$', logout, {'next_page': 'account:login'}, name='logout'),
     url(r'^signin/$', views.signin, name='signin'),
+    url(r'^profile/$', views.profile, name='profile'),
+    url(r'^edit/$', views.edit_profile, name='edit_profile'),
 ]
--- account/views.py
+++ account/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
-from django.views.generic import CreateView
-
+from django.views.generic import CreateView, DetailView,UpdateView
+from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import User
 from .forms import UserCreationForm
 
@@ -12,4 +12,28 @@ class CreateUserView(CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy('account:login')
 
+
+class Porfile(DetailView,LoginRequiredMixin):
+    model = User
+    template_name = 'profile.html'
+
+    def get_object(self):
+        return self.request.user
+
+class EditProfileVView(LoginRequiredMixin,UpdateView):
+    model = User
+    template_name = 'edit.html'
+    fields = ['username', 'name', 'email', 'avatar']
+    success_url = reverse_lazy('account:profile')
+
+    def get_object(self):
+        return self.request.user
+
+    def get_context_data(self, *args, **kwargs):
+        context = super(EditProfileVView, self).get_context_data(**kwargs)
+        context['avatars'] = ['default', 'elyse', 'eve', 'kristy', 'lena', 'mark', 'matthew', 'molly', 'rachel']
+        return context
+
 signin = CreateUserView.as_view()
+profile = Porfile.as_view()
+edit_profile = EditProfileVView.as_view()
